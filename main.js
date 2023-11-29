d3fbe127418d42754dd49aaaae1d649d16804ef5
LXtutui <113383859+LXtutui@users.noreply.github.com>
2023-11-29 16:30:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var video="";
 var Status="";
 var canvas;
 var detector_obj;
+var objects=[];
+var confidence;
+
 function preload(){
     video=createVideo("video.mp4");
     video.hide();
@@ -14,6 +17,19 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(Status!=""){
+        detector_obj.detect(video, dados_obtidos);
+        for(var i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="status: objetos detectados"
+            document.getElementById("num").innerHTML="quantidade de objetos detectados: "+ objects.length;
+            fill("purple");
+            confidence=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+confidence+"%", objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("purple");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function iniciar(){
@@ -27,4 +43,14 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function dados_obtidos(error, results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        objects=results;
+    }
+
 }
